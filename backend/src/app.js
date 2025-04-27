a1dfe80e69c56b37f3a530ab2b3f4aee52a66dc2
eternal02.oct <eternal02.oct@gmail.com>
2025-04-27 07:04:47 +0000

    Basic auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get(
-  "/",
-  [(req, res, next) => {
-    console.log("response will be send from here");
-    next();
-    res.send("Hi User");
-  },
-  (req, res) => {
-    console.log("test");
-  }]
-);
+app.get("/admin/getAllUser", (req, res) => {
+  let token = "xyz1";
+  if (token === "xyz") {
+    res.send("All user data is received");
+  } else {
+    res.status(401).send("User is not authriosed.");
+  }
+});
+
+app.delete("/admin/delete", (req, res) => {
+  let token = "xyz1";
+  if (token === "xyz") res.send("One user data is get deleted");
+  else res.status(401).send("User is not authriosed.");
+});
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+  console.log(`Server is ruunning on http://localhost/8000`);
 });
