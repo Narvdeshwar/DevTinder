c52c82a139cc61dee19173618ab77ab7c468c924
eternal02.oct <eternal02.oct@gmail.com>
2025-04-29 12:45:59 +0000

    Created the API for finding the all user and one user by emailId

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,11 @@ const User = require("./models/user.model");
 const app = express();
 
 app.use(express.json());
-// signup api
 
+// API for USER SIGNUP
 app.post("/signup", async (req, res) => {
-  
   try {
     const user = new User(req.body);
-    console.log("sending the user data", user);
     const data = await user.save();
     res
       .status(201)
@@ -19,6 +17,34 @@ app.post("/signup", async (req, res) => {
     res.status(500).send("Unable to send the data", error.message);
   }
 });
+
+// API for finding One User
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.email;
+  try {
+    let isEmailExit = await User.findOne({ email: userEmailId });
+    if (!isEmailExit) {
+      res.send("User doesn't not exits");
+    }
+    res.send(isEmailExit);
+  } catch (error) {
+    res.status(401).send("Something happens at server end.");
+  }
+});
+
+// API for finding the all users
+app.get('/feed',async(req,res)=>{
+  try {
+    const allUserFeed=await User.find({});
+    if(!allUserFeed){
+      res.send({message:"Currently no user exits"})
+    }
+    res.send(allUserFeed)
+  } catch (error) {
+    res.status(401).send("Something error happens at Server!")
+  }
+
+})
 
 connectDB()
   .then(() => {
