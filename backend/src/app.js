02496db60522d8e4758905901cfbad43d78ae04e
eternal02.oct <eternal02.oct@gmail.com>
2025-04-30 03:59:20 +0000

    updating user details api

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,19 +33,45 @@ app.get("/user", async (req, res) => {
 });
 
 // API for finding the all users
-app.get('/feed',async(req,res)=>{
+app.get("/feed", async (req, res) => {
   try {
-    const allUserFeed=await User.find({});
-    if(!allUserFeed){
-      res.send({message:"Currently no user exits"})
+    const allUserFeed = await User.find({});
+    if (!allUserFeed) {
+      res.send({ message: "Currently no user exits" });
     }
-    res.send(allUserFeed)
+    res.send(allUserFeed);
   } catch (error) {
-    res.status(401).send("Something error happens at Server!")
+    res.status(401).send("Something error happens at Server!");
   }
+});
 
-})
+// API for updating the user details
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    console.log("user here", user);
+    if (!user) {
+      res.send("Unable to update the data");
+    }
+    res.send("User data updated successfully");
+  } catch (error) {
+    res.status(401).send("Something went wrong");
+  }
+});
 
+// deleting the user data
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    // console.log("UserId", userId);
+    const id = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(401).send("Something went wrong");
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connected successfully.");
