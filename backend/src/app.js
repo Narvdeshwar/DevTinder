c6d6894192ed82584005800183d2ae80ad108b1b
eternal02.oct <eternal02.oct@gmail.com>
2025-05-01 09:47:54 +0000

    Data validation and data sanitization

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,87 +1,118 @@
-const express = require("express");
-const connectDB = require("./config/connectDB");
-const User = require("./models/user.model");
-const app = express();
+const express = require('express')
+const connectDB = require('./config/connectDB')
+const User = require('./models/user.model')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 // API for USER SIGNUP
-app.post("/signup", async (req, res) => {
+app.post('/signup', async (req, res) => {
   try {
-    const user = new User(req.body);
-    const data = await user.save();
+    const user = new User(req.body)
+    const data = await user.save()
     res
       .status(201)
-      .json({ message: "You have successfully SignUp", data: data });
-  } catch (error) {
-    res.status(500).send("Unable to send the data", error.message);
+      .json({ message: 'You have successfully SignUp', data: data })
+  } catch (err) {
+    res.status(500).send('Unable to send the data ' + err.message)
   }
-});
+})
 
 // API for finding One User
-app.get("/user", async (req, res) => {
-  const userEmailId = req.body.email;
+app.get('/user', async (req, res) => {
+  const userEmailId = req.body.email
   try {
-    let isEmailExit = await User.findOne({ email: userEmailId });
+    let isEmailExit = await User.findOne({ email: userEmailId })
     if (!isEmailExit) {
-      res.send("User doesn't not exits");
+      res.send("User doesn't not exits")
     }
-    res.send(isEmailExit);
+    res.send(isEmailExit)
   } catch (error) {
-    res.status(401).send("Something happens at server end.");
+    res.status(401).send('Something happens at server end.')
   }
-});
+})
 
 // API for finding the all users
-app.get("/feed", async (req, res) => {
+app.get('/feed', async (req, res) => {
   try {
-    const allUserFeed = await User.find({});
+    const allUserFeed = await User.find({})
     if (!allUserFeed) {
-      res.send({ message: "Currently no user exits" });
+      res.send({ message: 'Currently no user exits' })
     }
-    res.send(allUserFeed);
+    res.send(allUserFeed)
   } catch (error) {
-    res.status(401).send("Something error happens at Server!");
+    res.status(401).send('Something error happens at Server!')
   }
-});
+})
 
 // API for updating the user details
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
-  const data = req.body;
+app.patch('/user/:userId', async (req, res) => {
+  const userId = req.params?.userId
+  // console.log("UserId receive from the header" + userId);
+  const data = req.body
   try {
-    const user = await User.findByIdAndUpdate({ _id: userId }, data);
-    console.log("user here", user);
-    if (!user) {
-      res.send("Unable to update the data");
+    const allowedUpdates = [
+      'password',
+      'gender',
+      'shortBio',
+      'skills',
+      'profileUrl'
+    ]
+    if (data?.skills.length > 10) {
+      console.log("Error skills can't be more than 10")
+      throw new Error("Skills can't be more than 10")
     }
-    res.send("User data updated successfully");
+
+    const isAllowed = Object.keys(data).every(val =>
+      allowedUpdates.includes(val)
+    )
+
+    console.log('fileds that are allowed for upadet', isAllowed)
+
+    if (!isAllowed) {
+      throw new Error('Unable to update the user data')
+      // res.status(400).send("Unable to update the data");
+      //  if i will writhe this line then i get this erro: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client because our catch block has been already send the error to the client and we are sending error twice so make sure try to send the single response is try block and single res send in the catch block
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: 'after',
+      runValidators: true
+    })
+
+    console.log('user here', user)
+
+    if (!user) {
+      res.send('Unable to update the data')
+    }
+
+    res.send('User data updated successfully')
   } catch (error) {
-    res.status(401).send("Something went wrong");
+    res.status(401).send('Data is not updated')
   }
-});
+})
 
 // deleting the user data
-app.delete("/user", async (req, res) => {
+app.delete('/user', async (req, res) => {
   try {
-    const userId = req.body.userId;
+    const userId = req.body.userId
     // console.log("UserId", userId);
-    const id = await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully");
+    const id = await User.findByIdAndDelete(userId)
+    res.send('User deleted successfully')
   } catch (error) {
-    res.status(401).send("Something went wrong");
+    res.status(401).send('Something went wrong')
   }
-});
+})
 connectDB()
   .then(() => {
-    console.log("Database connected successfully.");
+    console.log('Database connected successfully.')
     app.listen(8000, () => {
-      console.log("Server is running on the port 8000");
-    });
+      console.log('Server is running on the port 8000')
+    })
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(
-      "Unable to connect with database Kindly contact Administartor.",
+      'Unable to connect with database Kindly contact Administartor.',
       err
-    );
-  });
+    )
+  })
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,22 +1,56 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "First Name is required."],
-    minLength: [3, "First Name lenght must be greater than 3 charcter."],
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(val) {
+        if (!["male", "female", "others"].includes(val)) {
+          throw new Error("Select gender either male,female or others");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      max: 85,
+    },
+    shortBio: {
+      type: String,
+      default: "This is user default bio",
+    },
+    skills: {
+      type: [String],
+    },
+    profileUrl: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1511367461989-f85a21fda167?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required."],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required."],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
